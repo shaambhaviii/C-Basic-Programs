0e66d8934497a4c7f37237128fbef59727c59423
Shambhavi <120776110+shaambhaviii@users.noreply.github.com>
2026-03-11 16:01:20 +0530

    Create hello_world.c

---
+++ basic_io/hello_world.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+  printf("Hello World,Shamv here ...very nice to meet you\n");
+  //since its the little tradition used in coding so why not start with this program!!
+}
