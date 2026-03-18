71beef30811aa147aa880e59d79e55492926e721
Shambhavi <120776110+shaambhaviii@users.noreply.github.com>
2026-03-18 17:28:25 +0530

    Update hello_world.c

--- basic_io/hello_world.c
+++ basic_io/hello_world.c
@@ -1,6 +1,9 @@
+//Program : Hello World
+// Part of shamv's C programs
+//since its the little tradition used in coding so why not start with this program!!
 #include<stdio.h>
 int main(){
   printf("Hello World,Shamv here ...very nice to meet you\n");
-  //since its the little tradition used in coding so why not start with this program!!
+  
   return 0;
 }
