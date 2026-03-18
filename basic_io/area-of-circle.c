cd19e80d3431f0ab510bf1d632755219a1a207eb
Shambhavi <120776110+shaambhaviii@users.noreply.github.com>
2026-03-18 18:17:35 +0530

    Create area-of-circle.c

---
+++ basic_io/area-of-circle.c
@@ -0,0 +1,12 @@
+//Program name: Area of circle
+//Part of shamv's C programs
+
+#include<stdio.h>
+int main(){
+    int r,area;
+    printf("enter radius: ");
+    scanf("%d",&r);
+    area= 3.14 *r*r;
+    printf("Area of  circle is %d",area);
+    return 0;
+}
